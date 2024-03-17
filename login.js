892b5a57f153dfa762e9c1a8a29ecc4108b98346
vivekkurane <94759939+vivekkurane@users.noreply.github.com>
2024-03-17 20:39:31 +0530

    Update login.js

--- login.js
+++ login.js
@@ -6,6 +6,9 @@ const createPassword = document.getElementById('createPassword');
 const userName = document.getElementById('userName');
 const userId = document.getElementById('userId');
 const signUp = document.getElementById('signUp');
+const signIn = document.getElementById('signIn');
+const signEmail = document.getElementById('signEmail');
+const signinPass = document.getElementById('signinPass');
 
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
@@ -15,14 +18,6 @@ loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
 
-// function signUp(){
-//     let pass = createPassword.value;
-//     let email = userId.value;
-//     let username = userName.value;
-
-//     console.log( pass , email, username);
-
-// }
 
 signUp.addEventListener('click', () => {
     let pass = createPassword.value;
@@ -30,4 +25,13 @@ signUp.addEventListener('click', () => {
     let username = userName.value;
 
     console.log( pass , email, username);
+    alert(`You have successfully created new account` )
 })
+
+signIn.addEventListener('click', () => {
+    let pass = signinPass.value;
+    let email = signEmail.value;
+
+    alert(`You have successfully signed in` );
+})
+
